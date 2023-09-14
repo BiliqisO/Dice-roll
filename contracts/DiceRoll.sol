6c598703bfaa1bf896b4268467d61c4ccb989ce5
BiliqisO <biliqisonikoyi@gmail.com>
2023-09-15 00:49:25 +0100

    updates

--- contracts/DiceRoll.sol
+++ contracts/DiceRoll.sol
@@ -2,57 +2,51 @@
 pragma solidity ^0.8.9;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 contract DiceRoll is VRFConsumerBaseV2 {
     // event CoinFlipRequest(uint requestId);
-    // event CoinFlipResult(uint requestId, bool didWin);  
+    // event CoinFlipResult(uint requestId, bool didWin);
     VRFCoordinatorV2Interface immutable vrfCoordinatorV2;
-     event DiceRolled(uint256 requestId, uint32 numWords);
+   
+    event RandomWords(uint256 requestId, uint256[] randomWords);
     event DiceResult(uint256 requestId, uint256[] randomWords);
-    
+
     bytes32 immutable keyHash;
-    uint64 immutable subcriptionId ;
+    uint64 immutable subcriptionId;
     uint32 constant callBackGasLimit = 1_000_000;
     uint32 constant numWords = 1;
-    uint16 constant requestConfirmations = 2;
-    uint constant  diceSides = 6;
+    uint16 constant requestConfirmations = 3;
+    uint constant diceSides = 6;
     uint public randomResult;
-    address _VRFCoordinator =  0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
-  
-   
-    //VRF Coordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D
-    //LINK TOKEN = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+    address _VRFCoordinator = 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625;
 
-  
-    constructor( uint64 _subscriptionId ) payable VRFConsumerBaseV2( _VRFCoordinator){
-    vrfCoordinatorV2 = VRFCoordinatorV2Interface(_VRFCoordinator);
-    keyHash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
-    subcriptionId = _subscriptionId;
-    
-   
+    constructor(uint64 _subscriptionId) VRFConsumerBaseV2(_VRFCoordinator) {
+        vrfCoordinatorV2 = VRFCoordinatorV2Interface(_VRFCoordinator);
+        keyHash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
+        subcriptionId = _subscriptionId;
     }
-    function rollDice() external   returns (uint256 requestId){
-      //submit VRF request 
-      requestId = vrfCoordinatorV2.requestRandomWords(
-        //price per gas
-        keyHash,
-        subcriptionId,
-        requestConfirmations,
-        //max gas amount
-        callBackGasLimit,
-        numWords);
-    
- 
-    
-    // emit CoinFlipRequest(requestId);
-    // return requestId;
-    }
-  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal   override {
-        // require(statuses[requestId].fees > 0, "LINK tokens not enough");
-       randomResult  =( randomWords[0] % diceSides) + 1;
-       emit DiceResult(requestId, randomWords);
 
+    function rollDice() external returns (uint256 requestId) {
+        //submit VRF request
+        requestId = vrfCoordinatorV2.requestRandomWords(
+            keyHash,
+            subcriptionId,
+            requestConfirmations,
+            //max gas amount
+            callBackGasLimit,
+            numWords
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        randomResult = (randomWords[0] % diceSides) + 1;
+        emit RandomWords(requestId, randomWords);
+    }
+
+    function diceResult() public view returns (uint) {
+        return randomResult;
+    }
 }
-function    diceResult() public view returns(uint){
-return randomResult;
-}
- }
--- scripts/diceroll.ts
+++ scripts/diceroll.ts
@@ -3,15 +3,22 @@ import { ethers } from "hardhat";
 async function main() {
   const subscriptionId = "5261";
 
-  const lock = await ethers.deployContract("DiceRoll", [subscriptionId]);
+  const diceroll = await ethers.deployContract("DiceRoll", [subscriptionId]);
 
-  await lock.waitForDeployment();
+  await diceroll.waitForDeployment();
+  const callCooordinator = await ethers.getContractAt(
+    "VRFCoordinatorV2Interface",
+    "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625"
+  );
+  const tx = await callCooordinator.addConsumer(
+    subscriptionId,
+    diceroll.target
+  );
+  console.log(await tx.wait());
 
-  console.log(`deployed to ${lock.target}`);
+  console.log(`deployed to ${diceroll.target}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
